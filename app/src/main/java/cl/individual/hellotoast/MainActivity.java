de034bd9f432f6a80ebe42c0ae4ff3c032bfa367
Emilia <correoalcantaraemilia@gmail.com>
2023-06-19 20:50:14 -0400

    Incorporacion funcion boton show toast

--- app/src/main/java/cl/individual/hellotoast/MainActivity.java
+++ app/src/main/java/cl/individual/hellotoast/MainActivity.java
@@ -3,6 +3,10 @@ package cl.individual.hellotoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initListeners();
+
+    }
+
+    private void initListeners() {
+        Button botonSuperior = findViewById(R.id.botonShowToast);
+        Button botonInferior = findViewById(R.id.botonCount);
+        TextView textCentral = findViewById(R.id.textViewContador);
+        botonSuperior.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Hello Toast!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
